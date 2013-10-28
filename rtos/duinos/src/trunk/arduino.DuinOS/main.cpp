df8dc7de0c9d582e82a0cf39db4ea69c870d38cc
Zhamak Dehghani <zhamakd@yahoo.com>
2013-10-28 23:19:52 +1100

    added duinos src here for now

---
+++ rtos/duinos/src/trunk/arduino.DuinOS/main.cpp
@@ -0,0 +1,30 @@
+#include <Arduino.h>
+
+unsigned portBASE_TYPE mainLoopPriority;
+
+void main_Task(void *pvParameters)
+{
+	for (;;) {
+		loop();
+		if (serialEventRun) serialEventRun();
+	}
+        
+}
+
+int main(void) 
+{
+	mainLoopPriority = LOW_PRIORITY;
+
+	init();
+#if defined(USBCON)
+	USB.attach();
+#endif
+	setup();
+
+	xTaskCreate(main_Task, (signed portCHAR *) "main", configMINIMAL_STACK_SIZE, NULL, mainLoopPriority, NULL);
+	vTaskStartScheduler();
+
+	for(;;);
+
+	return 0;
+}
